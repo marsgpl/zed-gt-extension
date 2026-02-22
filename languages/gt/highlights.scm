20a21d92a603e226fc9db8f486a8578881de230d
Iurii Belobeev <marsgpl@gmail.com>
2026-02-22 20:53:15 +0700

    regenerate grammar

--- languages/gt/highlights.scm
+++ languages/gt/highlights.scm
@@ -1,17 +1,26 @@
-; Invalid (default fallback)
-(node) @comment
-(key) @comment
-(value) @comment
-
-; Valid (override if matches pattern)
+; Valid node
 ((node) @keyword
  (#match? @keyword "^[a-zA-Z0-9_ -]+$"))
 
+; Invalid node
+((node) @comment
+ (#not-match? @comment "^[a-zA-Z0-9_ -]+$"))
+
+; Valid key
 ((key) @property
  (#match? @property "^[a-zA-Z0-9_ -]+$"))
 
+; Invalid key
+((key) @comment
+ (#not-match? @comment "^[a-zA-Z0-9_ -]+$"))
+
+; Valid value
 ((value) @keyword
  (#match? @keyword "^[a-zA-Z0-9_ -]+$"))
+
+; Invalid value
+((value) @comment
+ (#not-match? @comment "^[a-zA-Z0-9_ -]+$"))
 
 ":" @punctuation.delimiter
 (comment) @comment
