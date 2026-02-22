20c146a05eb0c78a53e6078fb61f56d02d02f0ea
Iurii Belobeev <marsgpl@gmail.com>
2026-02-22 21:21:05 +0700

    regenerate grammar

--- languages/gt/highlights.scm
+++ languages/gt/highlights.scm
@@ -1,26 +1,26 @@
 ; Valid node
 ((node) @keyword
- (#match? @keyword "^[a-zA-Z0-9_-]([a-zA-Z0-9_ -]*[a-zA-Z0-9_-])?$"))
+ (#match? @keyword "^[a-zA-Z0-9_-]+( [a-zA-Z0-9_-]+)*$"))
 
 ; Invalid node
 ((node) @comment
- (#not-match? @comment "^[a-zA-Z0-9_-]([a-zA-Z0-9_ -]*[a-zA-Z0-9_-])?$"))
+ (#not-match? @comment "^[a-zA-Z0-9_-]+( [a-zA-Z0-9_-]+)*$"))
 
 ; Valid key
 ((key) @property
- (#match? @property "^[a-zA-Z0-9_-]([a-zA-Z0-9_ -]*[a-zA-Z0-9_-])?$"))
+ (#match? @property "^[a-zA-Z0-9_-]+( [a-zA-Z0-9_-]+)*$"))
 
 ; Invalid key
 ((key) @comment
- (#not-match? @comment "^[a-zA-Z0-9_-]([a-zA-Z0-9_ -]*[a-zA-Z0-9_-])?$"))
+ (#not-match? @comment "^[a-zA-Z0-9_-]+( [a-zA-Z0-9_-]+)*$"))
 
 ; Valid value
 ((value) @keyword
- (#match? @keyword "^[a-zA-Z0-9_-]([a-zA-Z0-9_ -]*[a-zA-Z0-9_-])?$"))
+ (#match? @keyword "^[a-zA-Z0-9_-]+( [a-zA-Z0-9_-]+)*$"))
 
 ; Invalid value
 ((value) @comment
- (#not-match? @comment "^[a-zA-Z0-9_-]([a-zA-Z0-9_ -]*[a-zA-Z0-9_-])?$"))
+ (#not-match? @comment "^[a-zA-Z0-9_-]+( [a-zA-Z0-9_-]+)*$"))
 
 ":" @punctuation.delimiter
 (comment) @comment
